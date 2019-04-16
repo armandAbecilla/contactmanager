3ae83e68612a2dd487f6e385f9a95acd4ba53e3d
Armand Abecilla <abecilla.armand@gmail.com>
2019-04-17 00:42:05 +0800

    Added and implemented bootstrap styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from "react";
 import Contact from "./components/Contact";
 import Header from "./components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header branding="Contact Manager" />
-        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
-        <Contact
-          name="Karen Smith"
-          email="karen@gmail.com"
-          phone="333-333-3333"
-        />
+        <div className="container">
+          <Contact
+            name="John Doe"
+            email="jdoe@gmail.com"
+            phone="555-555-5555"
+          />
+          <Contact
+            name="Karen Smith"
+            email="karen@gmail.com"
+            phone="333-333-3333"
+          />
+        </div>
       </div>
     );
   }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,11 +11,11 @@ class Contact extends Component {
   render() {
     const { name, email, phone } = this.props;
     return (
-      <div>
-        <h1>{name}</h1>
-        <ul>
-          <li>Email: {email}</li>
-          <li>Phone: {phone}</li>
+      <div className="card card-body mb-3">
+        <h4>{name}</h4>
+        <ul className="list-group">
+          <li className="list-group-item">Email: {email}</li>
+          <li className="list-group-item">Phone: {phone}</li>
         </ul>
       </div>
     );
