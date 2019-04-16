6a6be96a02dbc3fc655e4a8d59055332baa54af3
Armand Abecilla <abecilla.armand@gmail.com>
2019-04-17 01:00:05 +0800

    Further destructured, fixed proptypes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,13 +3,11 @@ import PropTypes from "prop-types";
 
 class Contact extends Component {
   static propTypes = {
-    name: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired,
-    phone: PropTypes.string.isRequired
+    contact: PropTypes.object.isRequired
   };
 
   render() {
-    const { name, email, phone } = this.props;
+    const { name, email, phone } = this.props.contact;
     return (
       <div className="card card-body mb-3">
         <h4>{name}</h4>
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -30,12 +30,7 @@ class Contacts extends Component {
     return (
       <div>
         {contacts.map(contact => (
-          <Contact
-            key={contact.id}
-            name={contact.name}
-            email={contact.email}
-            phone={contact.phone}
-          />
+          <Contact key={contact.id} contact={contact} />
         ))}
       </div>
     );
